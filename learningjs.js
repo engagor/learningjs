110a5287f71209305494c341caac63e85812b6ee
toinne <anthony.meirlaen@gmail.com>
2018-12-09 16:31:15 +0100

    make relevant classes better accessible

--- learningjs.js
+++ learningjs.js
@@ -7,7 +7,16 @@
 //  email: yandongl _at_ cs.cmu.edu
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-let Tree = require('./tree.js');
-let Logistic = require('./logistic.js');
+const Tree = require('./tree.js');
+const Logistic = require('./logistic.js');
+const DataUtil = require('./dataUtil.js');
+const TreeModel = require('./treeModel.js');
+const LogisticModel = require('./logisticModel.js');
 
-module.exports = {Tree: Tree, Logistic: Logistic};
+module.exports = {
+    Tree: Tree,
+    Logistic: Logistic,
+    DataUtil: DataUtil,
+    TreeModel: TreeModel,
+    LogisticModel: LogisticModel
+};
