fff5faf5bda50dda346a4a8dd36a8739468203f2
toinne <anthony.meirlaen@gmail.com>
2018-12-09 16:01:15 +0100

    .env to make logging optional

--- debug.js
+++ debug.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
+
 class Debug {
     static debugp(msg, depth) {
+        if (process.env['VERBOSE'] === true) {
+            return;
+        }
+
         let s = '';
         for (let i = 0; i < depth * 2; i++) {
             s += ' ';
